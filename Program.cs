5e3af18d600e7abec07700f6d0b9969a9aa763a5
Dre mf Randaci <drerandaci@gmail.com>
2018-01-11 12:41:04 -0600

    finished part 3

--- Program.cs
+++ Program.cs
@@ -11,38 +11,41 @@ namespace bangazon_01
             List<Department> departments = new List<Department>();
 
             // Create some instances
+            // Add derived departments to the list
             HumanResources hr = new HumanResources("HR", "Amy Schumer", 2);
             Development dev = new Development("Dev", "John Johnny", 2);
             Marketing marketing = new Marketing("Marketing", "Bill Billy", 4);
 
-            // Add derived departments to the list
             departments.Add(hr);
             departments.Add(dev);
-            departments.Add(marketing);
-
-            // Iterate over all items in the list and output a string
-            // representation of the class
-            foreach (Department d in departments)
-            {
-                Console.WriteLine($"{d.toString()}");
-                d.meet();
-            }
-
-            double baseBudget = 75000.00;
+            departments.Add(marketing);                                    
 
             // Some departments get the base $75,000.00 budget, but others
             // will be adjusted up or down depending on the logic you wrote
             // in each class.
-            foreach (Department d in departments)
-            {
-                d.SetBudget(baseBudget);
-                Console.WriteLine(d.getBudget());
-                Console.WriteLine();
-                Console.WriteLine($"{d.toString()}");                
-                Console.WriteLine();
-            }
+            // Iterate over all items in the list and output a string
+            // representation of the class
 
+            // double baseBudget = 75000.00;
+            // foreach (Department d in departments)
+            // {
+            //     d.SetBudget(baseBudget);
+            //     Console.WriteLine(d.getBudget());
+            //     Console.WriteLine();
+            //     Console.WriteLine($"{d.toString()}");                
+            //     Console.WriteLine();
+            // }
 
+            List<Employee> companions = new List<Employee>();
+            Employee dre = new Employee("Dre", "Randaci");
+            Employee jim = new Employee("Jim", "Randaci");
+            Employee brad = new Employee("Brad", "Randaci");
+            companions.Add(jim);
+            companions.Add(brad);
+            dre.eat();            
+            dre.eat("Crockpot Soup");            
+            dre.eat(companions);            
+            dre.eat("some goooooood shit", companions);            
 
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# bangazon-01
+# bangazon 01-04
+- Create a new class to represent an Employee.
+- It's constructor should accept two parameters.
+- - First name of employee
+- - Last name of employee
+- Define a method named eat() that will allow it to be invoked with the following four signatures.
+- - `eat()` - Will select a random restaurant name from a list of strings, print to console that the employee is at that restaurant, and also return the restaurant.
+- - `eat(string food)` - Will output that the employee ate that specific food at the office.
+- - `eat(List<Employee> companions)` - Will select a random restaurant name from a list of strings, print to console that the employee is at that restaurant, and also output the first name of each employee in the specified list.
+- - `eat(string food, List<Employee> companions)` - Will select a random restaurant name from a list of strings, print to console that the employee at that restaurant, and ordered the specified food, with the first name of the teammates specified in the list.
